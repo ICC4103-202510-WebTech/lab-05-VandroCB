207c697f1973437862cbc5c8b21b033d4a554273
ivan <iaceballos@miuandes.cl>
2025-05-02 19:55:04 -0400

    Validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :sent_chats, class_name: "Chat", foreign_key: "sender_id", dependent: :destroy
   has_many :received_chats, class_name: "Chat", foreign_key: "receiver_id", dependent: :destroy
   has_many :received_messages, through: :received_chats, source: :messages
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
