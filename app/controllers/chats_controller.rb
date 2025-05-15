ba9e1707818f3d821e1004d7ac5f01b7f97e061c
ivan <iaceballos@miuandes.cl>
2025-05-15 17:29:39 -0400

    create chat form added

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -7,7 +7,22 @@ class ChatsController < ApplicationController
     @chat = Chat.find(id)
   end
   def new
+    @chat = Chat.new
+    @users = User.all
   end
   def create
+    @chat = Chat.new(chat_params)
+    if @chat.save
+      redirect_to @chat, notice: "Chat created successfully."
+    else
+      @users = User.all
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:sender_id, :receiver_id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   has_many :received_chats, class_name: "Chat", foreign_key: "receiver_id", dependent: :destroy
   has_many :received_messages, through: :received_chats, source: :messages
 
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
