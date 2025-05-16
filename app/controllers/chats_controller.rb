ada85e3267707eec7e93103679ab66946bb447c7
IvanCB <iaceballos@miuandes.cl>
2025-05-16 13:50:59 -0400

    chat edit view added

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -20,6 +20,20 @@ class ChatsController < ApplicationController
     end
   end
 
+  def edit
+    @chat = Chat.find(params[:id])
+    @users = User.all
+  end
+
+  def update
+    @chat = Chat.find(params[:id])
+    if @chat.update(chat_params)
+      redirect_to @chat, notice: 'Chat was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def chat_params
